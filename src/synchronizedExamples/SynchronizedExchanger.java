ecb67841f5e8e42d6fcae212cf28db116952a483
CodenameOverlord <utsavkumardatta@Utsavs-MacBook-Air.local>
2025-01-15 17:24:10 +0530

    Synchronized example of objects

---
+++ src/synchronizedExamples/SynchronizedExchanger.java
@@ -0,0 +1,21 @@
+package synchronizedExamples;
+
+public class SynchronizedExchanger {
+    protected Object object = null;
+    public synchronized void setObject(Object object){
+        this.object = object;
+    }
+    public synchronized Object getObject(){
+        return this.object;
+    }
+    public void setObj(Object object){
+        synchronized (this){
+            this.object = object;
+        }
+    }
+    public Object getObj(){
+        synchronized (this){
+            return this.object;
+        }
+    }
+}
